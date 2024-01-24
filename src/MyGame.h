6f4679bbb3934d6badd83e5fb65fbc274206f1df
Thomas Packham <72440768+TPackham1@users.noreply.github.com>
2024-01-24 01:44:47 +0000

    fixing file locations

--- src/MyGame.h
+++ src/MyGame.h
@@ -6,18 +6,32 @@
 #include <string>
 
 #include "SDL.h"
+#include "SDL_ttf.h"
 
 static struct GameData {
     int player1Y = 0;
+    int player1X = 0;
     int player2Y = 0;
+    int player2X = 0;
     int ballX = 0;
     int ballY = 0;
+    int powerUpX = 0;
+    int powerUpY = 0;
+    int ballRadius = 5;
 } game_data;
-
+static struct Scores {
+    int player1Score = 0;
+    int player2Score = 0;
+} scores;
 class MyGame {
-
+            // {x,y,w,h}
     private:
         SDL_Rect player1 = { 0, 0, 20, 60 };
+        SDL_Rect player2 = { 0, 0, 20, 60 };
+        int player1Score = 0;
+        int player2Score = 0;
+        
+        
 
     public:
         std::vector<std::string> messages;
@@ -26,7 +40,9 @@ class MyGame {
         void send(std::string message);
         void input(SDL_Event& event);
         void update();
+        void drawCircle(SDL_Renderer* renderer);
         void render(SDL_Renderer* renderer);
+
 };
 
 #endif
